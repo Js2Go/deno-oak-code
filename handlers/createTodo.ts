d19575692f53776682f2a1be1f23b52c2c76dd69
mazi <602281666@qq.com>
2020-07-27 23:10:06 +0800

    first commit

---
+++ handlers/createTodo.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'https://deno.land/x/oak/mod.ts'
+import { createTodo } from '../services/todos.ts'
+
+export default async ({
+  request,
+  response
+}: {
+  request: Request,
+  response: Response
+}) => {
+  if (!request.hasBody) {
+    response.status = 400
+    response.body = { msg: 'Invalid todo data' }
+    return
+  }
+
+  const { userId, title, completed = false } = await request.body().value
+
+  if (!userId || !title) {
+    response.status = 422
+    response.body = { msg: 'Incorrect todo data. userId and title are required' }
+    return
+  }
+
+  const todoId = await createTodo({ userId, title, completed })
+
+  response.body = { msg: "Todo created", todoId }
+}
